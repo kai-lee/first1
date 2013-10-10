360bc4a1a9be1db77d726d191771c35d652f9691
kai-lee <bbruesch@mail.csuchico.edu>
2013-10-09 17:44:03 -0700

    working on zipcode validation

--- ror/ABook/app/models/abook.rb
+++ ror/ABook/app/models/abook.rb
@@ -1,5 +1,6 @@
 class Abook < ActiveRecord::Base
 
+require 'zip_valid'
 
 #first name validation
 validates_presence_of :first_name
@@ -23,5 +24,9 @@ validates_format_of :zipcode, :with => /\A[0-9]{5}\z/
 #Favorite color validation
 validates_presence_of :fav_color
 
-
+def check_zip
+	if !(valid_zip.is_legal(self.zipcode))
+		errors.add :zipcode, "is not a valid zipcode"
+	end
+end
 end
